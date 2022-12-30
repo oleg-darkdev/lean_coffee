38a0691c0b5cd84041d1031e0699da5a9b607f27
DarkDev <darkdev_brand@riseup.net>
2022-12-30 14:55:12 +0200

    alfa v.

---
+++ src/components/PlainingPhase/Range.svelte
@@ -0,0 +1,14 @@
+<script>
+
+    import { Range, Label } from 'flowbite-svelte'
+    let stepValue=2.5
+
+
+    export let defaultTimeForEvent, minMin, maxMin, stepSetting;
+</script>
+
+
+<!-- {title} -->
+<!-- <Label>Pfujkjdjr yf[eq]</Label> -->
+<Range size="lg" id="range-steps" min={minMin} max={maxMin} bind:value={defaultTimeForEvent} step={stepSetting}/>
+<p>Minut: {defaultTimeForEvent}</p>
