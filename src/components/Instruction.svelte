0838582989283ccbf619d572d90d0304db41c68f
DarkDev <darkdev_brand@riseup.net>
2022-12-30 16:16:48 +0200

    fix back btn

--- src/components/Instruction.svelte
+++ src/components/Instruction.svelte
@@ -75,7 +75,9 @@ const showPlainingPhase = () => {
   
   <div class="w-full justify-center flex  ">
   <ButtonGroup class="space-x-px">
-    <Button on:click={() => step--} pill  color="alternative">back</Button>
+    {#if step != 0}
+      <Button on:click={() => step--} pill  color="alternative">back</Button>
+    {/if}
     <Button on:click={() => step++} pill  class="bg-freshGreen border-freshGreen">next <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 ml-2"><path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" /></svg></Button>
   </ButtonGroup>
   </div>
