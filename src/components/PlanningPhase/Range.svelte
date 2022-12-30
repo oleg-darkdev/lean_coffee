a529074c098c45938ae9a5e32938b44209f13221
DarkDev <darkdev_brand@riseup.net>
2022-12-30 17:50:35 +0200

    refactoring label

--- src/components/PlanningPhase/Range.svelte
+++ src/components/PlanningPhase/Range.svelte
@@ -1,14 +1,11 @@
 <script>
-
     import { Range, Label } from 'flowbite-svelte'
-    let stepValue=2.5
-
-
+ 
     export let defaultTimeForEvent, minMin, maxMin, stepSetting;
 </script>
 
 
-<!-- {title} -->
-<!-- <Label>Pfujkjdjr yf[eq]</Label> -->
+<label class="text-sm font-medium block text-gray-100">
+    Minut: {defaultTimeForEvent}
+</label>
 <Range size="lg" id="range-steps" min={minMin} max={maxMin} bind:value={defaultTimeForEvent} step={stepSetting}/>
-<p>Minut: {defaultTimeForEvent}</p>
